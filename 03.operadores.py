d9b7df538673298d5c906e8adfd6b2f60da74e35
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 09:27:39 -0500

    Operadores matematicas, e identificar el tipo de dato float

---
+++ 03.operadores.py
@@ -0,0 +1,24 @@
+num1 = 100
+num2 = 10
+
+resultado = num1 + num2
+print(resultado)
+
+resultado2 = num1 - num2
+print(resultado2)
+
+resultado3 = num1 * num2
+print(resultado3)
+
+resultado4 = num1 / num2
+print(resultado4)
+print(type(resultado4))
+
+resultado5 = num1 // num2
+print(resultado5)
+
+resultado6 = num1 % num2
+print(resultado6)
+
+resultado7 = num1 ** num2
+print(resultado7)
