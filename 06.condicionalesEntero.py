3f9cf99c752bf7f6e5dec2a12a1cb527d320909d
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 10:04:48 -0500

    Iniciar variable, para reusarla y no tener que comparar si es mayor a 19 en todo el codigo.

--- 06.condicionalesEntero.py
+++ 06.condicionalesEntero.py
@@ -1,6 +1,11 @@
 edad = int(input("¿Cual es tu edad? "))
+mayor_edad = False
 
 if edad>=18:
+    mayor_edad = True
     print("Usted es mayor de edad ")
 else:
     print("Eres menor de edad")
+
+if mayor_edad == True:
+    print("Eres mayor de edad")
