b461d2818fcc551b8020878b55215470f54cb15e
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 09:13:28 -0500

    Definir variables tipo str y mostrar en consola si es tipo entero o tipo string

--- 01.variables.py
+++ 01.variables.py
@@ -3,9 +3,14 @@ print(variable_string)
 
 variable_int = 4 + 5
 print(variable_int)
+print(type(variable_int))
 
 variable_boolean = False    
 print(variable_boolean)
 
 variable_boolean = True    
 print(variable_boolean)
+
+my_int_to_str_variable = str(variable_int)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
