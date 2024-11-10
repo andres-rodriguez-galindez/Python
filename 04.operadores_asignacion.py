dde61d5c575ba0820007a7826bfeff7527b243ea
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 09:36:19 -0500

    Realizar operaciones mas sencillas, usando los operadores y las variables definidas, cuando se conviente a floar abreviamos los numeros a 0, o depende de los decimales que agregamos definimos 2 o mas en la funcion.

---
+++ 04.operadores_asignacion.py
@@ -0,0 +1,15 @@
+num1 = 100
+num1 += 50 #agregar algun valor a la variable que ya esta definida
+
+print(num1)
+
+num1 -= 20
+print(num1)
+
+num1 *= 2 
+print(num1)
+
+num1 /= 3
+print(num1)
+print(f"{num1:.0f}") #Abreviar los numeros
+
