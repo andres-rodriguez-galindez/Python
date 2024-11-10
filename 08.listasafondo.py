9dc700ffa2bf799bb31f9418ac1c769b6c135b7a
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 10:19:24 -0500

    definir las posiciones para mostrar el dato segun la posición

---
+++ 08.listasafondo.py
@@ -0,0 +1,10 @@
+nombres = ["Luke","Obi","Chiwi"]
+print(type(nombres))
+
+nombres[1] = "Leila"
+print(nombres)
+
+print(nombres[0:2]) #como la funcion range
+
+rango = list(range(0,10))
+print(rango[0:2])
