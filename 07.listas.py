50f5a39bfa327dfa54feaa14c50172a0a4887f49
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 10:14:45 -0500

    Usar lista y concatenarlas, usar range para definir el rango que deseo mostrar.

---
+++ 07.listas.py
@@ -0,0 +1,29 @@
+nombres = ["Luke","Obi","Chiwi"]
+print(nombres)
+
+numeros = [54,214,20.2]
+print(numeros)
+
+booleans = [True,False]
+print(booleans)
+
+matrices = [[0,3],[3.9]]
+print(matrices)
+
+otros = ["Hola", 2, False]
+print(otros)
+
+#referencia a la posición que se encuentra el dato y siempre empieza en la posición 0
+
+print(nombres[1])
+
+print(nombres, numeros)
+
+print(nombres + numeros)
+
+nombre_y_numeros = nombres + numeros
+print(nombre_y_numeros)
+
+#Funcion range, para definir un rango de numeros
+rango = list(range(1,6))
+print(rango)
