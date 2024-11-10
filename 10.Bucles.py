2e1826a501cf68256f87827d41fa45a11545ab88
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 12:28:31 -0500

    Bluce For

---
+++ 10.Bucles.py
@@ -0,0 +1,20 @@
+#Bluce For
+nombres = ["Luke","Obi","Chiwi"]
+
+for nombre in nombres:
+    print("Hola ", nombre)
+
+
+for i in range(10):
+    print(i)
+
+for i in range(5,11):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)    
+
+numeros = (1, 4, 7)
+for i in numeros:
+    print(i)
+
