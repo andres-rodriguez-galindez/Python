1ea71c9ba0b3a902c42490db285cab64ad4cce3d
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 09:20:40 -0500

    Comparadores para identificar si es diferente, igual, igual o menor...

---
+++ 02.comparadores.py
@@ -0,0 +1,11 @@
+#dos es mayor a uno es verdadero
+print(2>1)
+#dos es menor a uno es falso
+print(2<1)
+#Compare si dos es igual a uno si o si
+print(2==1)
+print(2==2)
+#Simbolo diferente !
+print(2!=1)
+print(2>=1)
+print(2<=1)
