435311ce478fdb9fbc217f072bfe821133efeed8
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-11 10:36:31 -0500

    funciones un poco mas avanzado

---
+++ 16.funciones3.py
@@ -0,0 +1,8 @@
+empresas = [7,4,8,5,6,8,200,1000]
+
+def facturacion_media(empresas):
+    total = sum(empresas)
+    media = total / len(empresas)
+    return media
+resultado = facturacion_media(empresas)
+print("La facturacion media es de: ", resultado)
