a9e1ea5b04e1199fbf34667758c52a0d32c8f3d5
andres-rodriguez-galindez <andres.rodriguez.galindez@gmail.com>
2024-11-10 21:59:38 -0500

    Tabla del 7

--- 11.TablaMultiplicardos.py
+++ 11.TablaMultiplicardos.py
@@ -2,3 +2,7 @@
 for multiplicar in range (0,11):
     resultado = 2 * multiplicar
     print(f"{2} x {multiplicar} = {resultado}")
+
+for multiplicar in range (0,11):
+    resultado = 7 * multiplicar
+    print(f"{7} x {multiplicar} = {resultado}")
